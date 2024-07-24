fe04bd58aa625798b9c7542370a6ec9aa099e545
Juan Garcia <caml1@live.com>
2024-07-23 23:31:30 -0400

    Worked on the controller and router for dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,4 +6,74 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+// Middleware to validate order existence
+function dishExists(req, res, next) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find(dish => dish.id === dishId);
+    if (foundDish) {
+        res.locals.dish = foundDish;
+        return next();
+    }
+    next({
+        status: 404,
+        message: `Dish ID ${dishId} not found`,
+    });
+}
+
+// Create
+function create(req, res) {
+    const { data: { name, description,  price, image_url } = {} } = req.body;
+    const newDish = {
+        id: nextId(),
+        name,
+        description,
+        price,
+        image_url,
+    };
+    orders.push(newDish);
+    res.status(201).json({ data: newDish });
+}
+
+// Read
+function read(req, res) {
+    res.json({ data: res.locals.dish });
+}
+
+// Update
+function update(req, res) {
+    const dish = res.locals.dish;
+    const { data: { name, description,  price, image_url } = {} } = req.body;
+
+    // Update the dish
+    dish.name = name;
+    dish.description = description;
+    dish.price = price;
+    dish.image_url = image_url;
+
+    res.json({ data: dish });
+}
+
+// Delete
+function destroy(req, res) {
+    const { dishId } = req.params;
+    const index = dishes.findIndex(dish => dish.id === dishId);
+
+    if (index !== -1) {
+        dishes.splice(index, 1);
+        return res.sendStatus(204);
+    }
+    res.status(404).json({ error: `Dish ID ${dishId} not found` });
+}
+
+// List
+function list(req, res) {
+    res.json({ data: dishes });
+}
+
+module.exports = {
+    create,
+    read: [dishExists, read],
+    update: [dishExists, update],
+    delete: [dishExists, destroy],
+    list,
+};
